d901df27b4d202b268a7d92fe5614610e9b2c56a
Ruban Durai <77948100+RubanDurai@users.noreply.github.com>
2023-06-26 19:40:58 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,6 +3,6 @@ import os
 class Config:
     API_ID = int(os.environ.get("API_ID", "6596420"))
     API_HASH = os.environ.get("API_HASH", "d052e942057719c009374ea083a1a57d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "2068606974:AAGB9d62iBllI8MpAPZAtll5goQ23TB0PKU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6271078857:AAHHRqce3YD1REQ1jI2k-6fX_qdlYlhaoCw")
     ADMINS = int(os.environ.get("ADMINS", "1389078939"))
     UPDATES_CHANNEL = int(os.environ.get("UPDATES_CHANNEL", "-1001582342980"))
